b2f8d583838f8153de9f0b249adb29933100559e
golueb <golubevegor80@gmail.com>
2025-02-05 23:23:49 +0300

    added notes

--- image_board/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LogoutView, LoginView
-from .views import *
-
-urlpatterns = [
-    # Главная страница
-    path('', MainPageView.as_view(), name='home')
-]
